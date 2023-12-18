d1c26358d176c083f5382ac375d10f6fc14b5915
Ryan Goree <goree.ryan@gmail.com>
2023-12-18 15:47:05 -0600

    Refine error handling in resolver

--- packages/clide-js/src/utils/fs.ts
+++ packages/clide-js/src/utils/fs.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import { removeFileExtension } from './remove-file-extension';
 
 /**
  * Determine if a path is a directory without throwing an error
@@ -13,12 +14,46 @@ export function isDirectory(path: string): boolean {
 }
 
 /**
- * Determine if a path is a file without throwing an error
+ * Attempt to determine if a path is a file. If the path does not exist, check
+ * if the path with any of the provided fallback extensions is a file.
+ * @param path - The path to check.
+ * @param fallbackExtensions - The fallback extensions to check if the path
+ * does not exist.
+ * @group Utils
  */
-export function isFile(path: string): boolean {
+export function isFile(
+  path: string,
+  fallbackExtensions: string[] = ['.js', '.cjs', '.mjs', '.ts', '.cts', '.mts'],
+): boolean {
   try {
     return fs.statSync(path).isFile();
-  } catch {
+  } catch (err) {
+    // Forward any error that isn't a module not found error
+    if (!isModuleNotFoundError(err)) throw err;
+
+    // Check if the path with any of the fallback extensions is a file
+    for (const ext of fallbackExtensions) {
+      try {
+        return fs.statSync(`${removeFileExtension(path)}${ext}`).isFile();
+      } catch (err) {
+        // Forward any error that isn't a module not found error
+        if (!isModuleNotFoundError(err)) throw err;
+      }
+    }
+
+    // If none of the fallback extensions returned true, return false
     return false;
   }
+}
+
+/**
+ * Attempt to determine if an error is a module not found error.
+ * @group Utils
+ * @remarks
+ * This function is not guaranteed to be accurate in every environment and is
+ * meant to be used when the source of the error is known.
+ */
+function isModuleNotFoundError(err: unknown): boolean {
+  if (err && (err as any).code === 'ENOENT') return true;
+  return /(Cannot find module|not a directory, stat)/.test(String(err));
 }
