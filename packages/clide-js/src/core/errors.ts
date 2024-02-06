5bf6519a1396af72ba593a01f0d12564414e0537
Ryan Goree <goree.ryan@gmail.com>
2024-02-05 19:54:12 -0600

    Prevent broken errors

--- packages/clide-js/src/core/errors.ts
+++ packages/clide-js/src/core/errors.ts
@@ -12,6 +12,13 @@ export class ClideError extends Error {
   private _name: string;
 
   constructor(error: any) {
+    // Ensure the error can be converted into a primitive type which is required
+    // for the `Error` constructor.
+    try {
+      String(error);
+    } catch {
+      throw error;
+    }
     super(error?.message || error);
     this._name = `${ERROR_PREFIX}CLI Error`;
   }
