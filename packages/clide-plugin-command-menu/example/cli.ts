a9530210683dd9413238354d4c544dbcf66d7a66
Ryan Goree <goree.ryan@gmail.com>
2024-04-21 19:30:51 -0500

    Update pr workflow

--- packages/clide-plugin-command-menu/example/cli.ts
+++ packages/clide-plugin-command-menu/example/cli.ts
@@ -3,7 +3,7 @@ import { commandMenu } from '../src/command-menu.js';
 
 run({
   plugins: [
-    help,
+    help(),
     commandMenu({
       title: 'Command Menu',
       skip: (options) => !!options.help,
