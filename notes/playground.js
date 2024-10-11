fc77062f508083bdb3f1934b744ca462e2f1efc5
Ryan Goree <goree.ryan@gmail.com>
2024-10-11 16:08:27 -0500

    Format

--- notes/playground.js
+++ notes/playground.js
@@ -9,46 +9,46 @@ async function execute({ steps, data }) {
     // modify the data and continue to the next step if there is one, otherwise
     // return the data.
     result: async (data) => {
-      state.data = data
-      const nextHandler = steps[state.i + 1]
+      state.data = data;
+      const nextHandler = steps[state.i + 1];
       if (nextHandler) {
-        state.i++
-        return await nextHandler(Object.freeze({ ...state }))
+        state.i++;
+        return await nextHandler(Object.freeze({ ...state }));
       }
-      return data
+      return data;
     },
 
     // modify the data and continue to the next step if there is one, otherwise
     // throw an error.
     next: async (data) => {
-      const nextHandler = steps[state.i + 1]
-      if (!nextHandler) state.error(new Error(`No next step`))
-      state.data = data
-      state.i++
-      return await nextHandler(Object.freeze({ ...state }))
+      const nextHandler = steps[state.i + 1];
+      if (!nextHandler) state.error(new Error(`No next step`));
+      state.data = data;
+      state.i++;
+      return await nextHandler(Object.freeze({ ...state }));
     },
 
     // modify the data and return it, logging a warning if there were more steps
     end: (data) => {
-      state.data = data
+      state.data = data;
       if (steps[state.i + 1]) {
-        state.warn('Ending prematurely, remaining steps will be skipped.')
+        state.warn('Ending prematurely, remaining steps will be skipped.');
       }
-      return data
+      return data;
     },
 
     error: (error) => {
       // TODO: custom error logic
-      throw error
+      throw error;
     },
 
     warn: (warning) => {
       // TODO: custom error logic
-      throw console.warn(warning)
+      throw console.warn(warning);
     },
-  }
+  };
 
-  return state.next(data)
+  return state.next(data);
 }
 
 async function example() {
@@ -60,27 +60,27 @@ async function example() {
       ({ result, end }) => {
         const timeout = new Promise((_, reject) =>
           setTimeout(() => {
-            reject(end(new Error('timed out')))
+            reject(end(new Error('timed out')));
           }, 1000),
-        )
-        return Promise.race([result({ name: 'ryan' }), timeout])
+        );
+        return Promise.race([result({ name: 'ryan' }), timeout]);
       },
 
       ({ result, data }) => {
         return new Promise((resolve) => {
           setTimeout(() => {
-            resolve(result(data))
-          }, 500)
-        })
+            resolve(result(data));
+          }, 500);
+        });
       },
 
       ({ data, end }) => {
-        return end({ ...data, age: 34 })
+        return end({ ...data, age: 34 });
       },
     ],
-  })
-  console.log(data)
-  process.exit()
+  });
+  console.log(data);
+  process.exit();
 }
 
-example()
+example();
