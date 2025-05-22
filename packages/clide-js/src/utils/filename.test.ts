d56f391d714113753ffd1200974cc1431712696d
Ryan Goree <goree.ryan@gmail.com>
2025-05-21 21:44:28 -0500

    Delete repeated tests

--- packages/clide-js/src/utils/filename.test.ts
+++ packages/clide-js/src/utils/filename.test.ts
@@ -1,4 +1,8 @@
-import { formatFileName, parseFileName } from 'src/utils/filename';
+import {
+  formatFileName,
+  parseFileName,
+  removeFileExtension,
+} from 'src/utils/filename';
 import { describe, expect, it } from 'vitest';
 
 describe('filename', () => {
@@ -115,6 +119,26 @@ describe('filename', () => {
       // missing opening or closing bracket
       expect(parseFileName('param].ts')).toEqual(expected);
       expect(parseFileName('[param.ts')).toEqual(expected);
+    });
+  });
+
+  describe('remove-file-extension', () => {
+    it('removes the file extension from a string', () => {
+      expect(removeFileExtension('foo.ts')).toBe('foo');
+    });
+
+    it('leaves a string without a file extension unchanged', () => {
+      expect(removeFileExtension('foo')).toBe('foo');
+    });
+
+    it('only removes the last file extension from a string with multiple file extensions', () => {
+      expect(removeFileExtension('foo.test.ts')).toBe('foo.test');
+    });
+
+    it('ignores multiple dots in a file name', () => {
+      expect(removeFileExtension('[...foo].ts')).toBe('[...foo]');
+      expect(removeFileExtension('[...foo]')).toBe('[...foo]');
+      expect(removeFileExtension('foo..bar')).toBe('foo..bar');
     });
   });
 });
